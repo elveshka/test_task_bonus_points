cce00cb7427bfb60f9581fce8f1f1c01d1eb22e0
Elvek Goriaev <elvekgoryaev@gmail.com>
2023-10-05 17:01:04 +0300

    Add first point of the Plan.

---
+++ src/main/java/test/task_bonus_points/TaskBonusPointsApplication.java
@@ -0,0 +1,11 @@
+package test.task_bonus_points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBonusPointsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBonusPointsApplication.class);
+    }
+}
